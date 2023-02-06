414819126cbd5885e24f46f274a79c545b305522
Marc C <mcar176@gmail.com>
2023-02-06 19:35:12 +0100

    Code Ex 6.9

--- redux-anecdotes/src/AnecdoteList.js
+++ redux-anecdotes/src/AnecdoteList.js
@@ -2,11 +2,16 @@ import { useSelector, useDispatch } from "react-redux";
 import { addVote } from "./reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    if (filter === "") {
+      return anecdotes;
+    }
+    return anecdotes.filter((a) => a.content.includes(filter));
+  });
   const dispatch = useDispatch();
 
   const vote = (id) => {
-    console.log("vote", id);
+    //console.log("vote", id);
     dispatch(addVote(id));
   };
 
--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,10 +1,12 @@
 import AnecdoteForm from "./AnecdoteForm";
 import AnecdoteList from "./AnecdoteList";
+import Filter from "./Filter";
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <h2>create new</h2>
       <AnecdoteForm />
